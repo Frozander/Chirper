ed84c76ea7e518a0e9a8c8a90efd551fedc8bda8
Mustafa Batuhan Ceylan <ceylan.mustafabatuhan@gmail.com>
2020-10-23 09:07:48 +0300

    Removed "Home" Tab from Navbar

--- chirper/navigation.py
+++ chirper/navigation.py
@@ -7,12 +7,12 @@ nav = Nav()
 
 @nav.navigation('chirper_navbar')
 def create_navbar():
-    home_view = View('Home', 'index')
+    home_view = View('Chirper', 'index')   
     if  current_user.is_authenticated:
         logout_view = View('Logout', 'index') # index is placeholder
         return Navbar('Chirper', home_view, logout_view)
     else:
         login_view = View('Log In', 'auth.login')
         register_view = View('Register', 'auth.register')
-        return Navbar('Chirper', home_view, login_view, register_view)
+        return Navbar(home_view, login_view, register_view)
     
