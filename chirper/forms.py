c15549222b33229c2f6dd5fbc70479ec31d18528
Mustafa Batuhan Ceylan <ceylan.mustafabatuhan@gmail.com>
2020-10-23 08:09:33 +0300

    Added stylesheets for Login and Register pages

--- chirper/forms.py
+++ chirper/forms.py
@@ -3,16 +3,15 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired()], 
+    email = StringField('E-Mail', validators=[DataRequired()], 
                         render_kw={'placeholder': 'Enter E-Mail'})
-    password = PasswordField('password', validators=[DataRequired(), Length(max=80, message="Max password length is 80 characters.")],
+    password = PasswordField('Password', validators=[DataRequired(), Length(max=80, message="Max password length is 80 characters.")],
                              render_kw={'placeholder': 'Enter Password'})
-    remember_me = BooleanField('remember_me', default=False)
+    remember_me = BooleanField('Remember Me', default=False)
 
 # TODO: ADD REGISTER FORM
 class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), Email()], 
+    email = StringField('Email', validators=[DataRequired(), Email()], 
                         render_kw={'placeholder': 'Enter E-Mail'})
-    password = PasswordField('password', validators=[DataRequired(), Length(max=80, message="Max password length is 80 characters.")],
+    password = PasswordField('Password', validators=[DataRequired(), Length(max=80, message="Max password length is 80 characters.")],
                              render_kw={'placeholder': 'Enter Password'})
-    remember_me = BooleanField('remember_me', default=False)
